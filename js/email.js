7163a3b14f8f0861f83531d397830d2b33cfb5b9
João Gabriel Messi Lutz <tibepc@gmail.com>
2022-12-14 20:05:07 -0300

    implementando envio de email

---
+++ js/email.js
@@ -0,0 +1,16 @@
+function enviarEmail() {
+    const nome = el("#name").value;
+    const email = el("#email").value;
+    const assunto = el("#subject").value;
+    const mensagem = el("#message").value;
+
+    
+}
+
+function el(selector) {
+    return document.querySelector(selector);
+}
+
+function els(selector) {
+    return document.querySelectorAll(selector);
+}
